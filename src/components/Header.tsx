c3d044e55b806c9264c28466f9e9957422150afc
gunwoongPark <bung1438@gmail.com>
2022-10-17 15:42:58 +0900

    feat(header): 헤더 생

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,19 @@
+import styles from "./Header.module.css";
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <ul className={styles.tab_container}>
+        <li className={styles.tab_red}>RED</li>
+        <li className={styles.tab_orange}>ORANGE</li>
+        <li className={styles.tab_yellow}>YELLOW</li>
+        <li className={styles.tab_green}>GREEN</li>
+        <li className={styles.tab_blue}>BLUE</li>
+        <li className={styles.tab_navy}>NAVY</li>
+        <li className={styles.tab_purple}>PURPLE</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
