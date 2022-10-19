48f122d7f0180af1c9437157d57d3e78538dad47
gunwoongPark <bung1438@gmail.com>
2022-10-20 01:55:14 +0900

    feat: 동적 헤더 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,13 +9,47 @@ const Header = (props: { currentTab: string }) => {
   return (
     <Pub.Container>
       <ul className="tab-container">
-        <li className="tab-red">RED</li>
-        <li className="tab-orange">ORANGE</li>
-        <li className="tab-yellow">YELLOW</li>
-        <li className="tab-green">GREEN</li>
-        <li className="tab-blue">BLUE</li>
-        <li className="tab-navy">NAVY</li>
-        <li className="tab-purple">PURPLE</li>
+        <li className={`tab red ${props.currentTab === "RED" ? "active" : ""}`}>
+          RED
+        </li>
+        <li
+          className={`tab orange ${
+            props.currentTab === "ORANGE" ? "active" : ""
+          }`}
+        >
+          ORANGE
+        </li>
+        <li
+          className={`tab yellow ${
+            props.currentTab === "YELLOW" ? "active" : ""
+          }`}
+        >
+          YELLOW
+        </li>
+        <li
+          className={`tab green ${
+            props.currentTab === "GREEN" ? "active" : ""
+          }`}
+        >
+          GREEN
+        </li>
+        <li
+          className={`tab blue ${props.currentTab === "BLUE" ? "active" : ""}`}
+        >
+          BLUE
+        </li>
+        <li
+          className={`tab navy ${props.currentTab === "NAVY" ? "active" : ""}`}
+        >
+          NAVY
+        </li>
+        <li
+          className={`tab purple ${
+            props.currentTab === "PURPLE" ? "active" : ""
+          }`}
+        >
+          PURPLE
+        </li>
       </ul>
     </Pub.Container>
   );
@@ -39,30 +73,36 @@ const Pub = {
       align-items: center;
       height: 100%;
 
-      & .tab-red {
-        background: red;
-      }
-      & .tab-orange {
-        background: orange;
-      }
-      & .tab-yellow {
-        background: yellow;
-      }
-      & .tab-green {
-        background: green;
-        color: #fff;
-      }
-      & .tab-blue {
-        background: blue;
-        color: #fff;
-      }
-      & .tab-navy {
-        background: navy;
-        color: #fff;
-      }
-      & .tab-purple {
-        background: purple;
-        color: #fff;
+      & .tab {
+        &.active {
+          border-bottom: 2.5px solid black;
+        }
+
+        &.red {
+          background: red;
+        }
+        &.orange {
+          background: orange;
+        }
+        &.yellow {
+          background: yellow;
+        }
+        &.green {
+          background: green;
+          color: #fff;
+        }
+        &.blue {
+          background: blue;
+          color: #fff;
+        }
+        &.navy {
+          background: navy;
+          color: #fff;
+        }
+        &.purple {
+          background: purple;
+          color: #fff;
+        }
       }
     }
   `,
